aa460c0365ee844824024989ce635253c302536b
Sarasvati Dallos <saricata1234567890@gmail.com>
2025-02-24 20:04:54 -0500

    Update EsCriminal.pl

--- EsCriminal.pl
+++ EsCriminal.pl
@@ -2,3 +2,7 @@ nacionalidad_de(coronel_west, estados_unidos).
 enemigo_de(corea_del_sur, estados_unidos).
 vender_armas_a(coronel_west, corea_del_sur).
 
+criminal(X) :- 
+    nacionalidad_de(X, estados_unidos),
+    vender_armas_a(X, Pais),
+    enemigo_de(Pais, estados_unidos).
